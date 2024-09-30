b066b3100221fd1e19f77ae7338bcc1ed8e0111c
sbakhareva <snezhana.bakhareva@yandex.ru>
2024-09-30 12:30:27 +0300

    готовое дз

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,57 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        // Task #1
+        System.out.println("Задача №1");
+        int [] integers = new int [] {1, 2, 3};
+        double [] fractional = new double [] {1.57, 7.654, 9.986};
+        boolean[] truLse = new boolean [] {1 < 2, 2 > 3};
+
+        // Task #2
+        System.out.println("Задача №2");
+        System.out.println(Arrays.toString(integers));
+        System.out.println(Arrays.toString(fractional));
+        System.out.println(Arrays.toString(truLse));
+
+        // Task #3
+        System.out.println("Задача №3");
+        for (int i = 2; i <= integers.length && i >= 0; i--) {
+            System.out.print(integers[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int fr = 2; fr <= fractional.length && fr >= 0; fr--) {
+            System.out.print(fractional[fr]);
+            if (fr > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int tf = 1; tf <= truLse.length && tf >= 0; tf--) {
+            System.out.print(truLse[tf]);
+            if (tf > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        // Task #4
+        System.out.println("Задача №4");
+        for (int i = 0; i <= 2; i++) {
+            if (integers[i] % 2 == 0) {
+                System.out.print(integers[i]);
+            } else {
+                integers[i]++;
+                System.out.print((integers[i]));
+            }
+            if (i < integers.length - 1) {
+                System.out.print(", ");
+            }
+        }
         }
     }
